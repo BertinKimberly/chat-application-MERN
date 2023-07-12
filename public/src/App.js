40bddb978e41379359dc631921756e792218cf6b
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-07-12 21:07:31 +0200

    styled index.css

--- public/src/App.js
+++ public/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
+import SetAvatar from "./pages/SetAvatar";
 const App = () => {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Chat />} />
+        <Route path="/setAvatar" element={<SetAvatar/>} />
       </Routes>
     </BrowserRouter>
   );
--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
 require("dotenv").config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/auth", userRoutes);
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
